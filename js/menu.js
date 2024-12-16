75403372d4d87c747c33bf813bad85060843649c
Jeroni Juan <jjuanh@esliceu.net>
2024-12-16 10:40:33 +0100

    16/12/24

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function toggleMenu(menuId) {
+    const menu = document.getElementById(menuId);
+
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
